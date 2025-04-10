1b6b8f33ec1fd579480f882aea63edb503638857
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-10 17:01:29 +0530

    updated axiosInstance

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:5000/api' ,
-    baseURL: 'https://s-media-frontend.onrender.com/api' ,
+    baseURL: 'https://s-media-backend-yha2.onrender.com/api' ,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     }
 })
 
